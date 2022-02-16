683e93b8ce74d0463fbbe48e239ba07c2b429125
Denis_Koval <den_hope@mail.ru>
2022-02-16 03:11:15 +0300

    fix: fix randomize function

--- memory-game/src/js/_data.js
+++ memory-game/src/js/_data.js
@@ -3,7 +3,8 @@ import { getData } from "./_library";
 //Rundomize function
 export const getRandomPicture = () => {
   const cardData = getData();
-  const randomCardData = cardData.sort(() => Math.random() - 0, 5);
-  // console.log(randomCardData);
+  const randomCardData = cardData.sort(() => {
+    return Math.random() - 0.5;
+  });
   return randomCardData;
 };
