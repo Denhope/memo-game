c08c46ff7129da4a06b3b841c04d30775c33ab2a
Denis_Koval <den_hope@mail.ru>
2022-02-16 18:18:56 +0300

    feat: add timer

--- memory-game/src/js/_gameBoard.js
+++ memory-game/src/js/_gameBoard.js
@@ -1,9 +1,18 @@
 import { checkCards } from "./_gameLogic";
 import { getRandomPicture } from "./_data";
+import { timer } from "./_timer";
+import { setStepCounter } from "./_scoreCounter";
+import { getScoreCounter } from "./_scoreCounter";
 
 // create game field
 export function createBoard() {
+  let steps = 0;
+  const stepCounter = document.querySelector(".step_counter");
+
+  const body = document.querySelector("body");
   const gameBoard = document.querySelector(".game-board__wrapper");
+  const startPlayButton = document.querySelector(".start-play");
+  const startWindow = document.querySelector(".start-window");
   const cardData = getRandomPicture();
   cardData.forEach((element) => {
     const card = document.createElement("div");
@@ -22,6 +31,15 @@ export function createBoard() {
     card.addEventListener("click", (evt) => {
       card.classList.toggle("card__toggle");
       checkCards(evt);
+      steps++;
+      stepCounter.innerHTML = steps;
+    });
+
+    startPlayButton.addEventListener("click", (evt) => {
+      startWindow.classList.add("start-window__hidden");
+      body.classList.remove("body_lock");
+      gameBoard.style.pointerEvents = "auto";
+      timer();
     });
   });
 }
