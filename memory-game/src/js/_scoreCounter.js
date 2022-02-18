e697239c425ad9a3b0cab9a0942fdedf88c3172e
Denis_Koval <den_hope@mail.ru>
2022-02-18 22:05:25 +0300

    fix: fix score

--- memory-game/src/js/_scoreCounter.js
+++ memory-game/src/js/_scoreCounter.js
@@ -3,15 +3,17 @@ const scoreCounterField = document.querySelector(".score_counter");
 let score = 0;
 
 const scoreCounterHigh = () => {
-  score = score + 5;
+  score = score + 10;
   scoreCounterResult.innerHTML = score;
   scoreCounterField.innerHTML = score;
   return score;
 };
 
 const scoreCounterLow = () => {
-  if (score > 0) {
+  if (score > 1) {
     score = score - 2;
+  } else if ((score = 1)) {
+    score = score - 1;
   }
 
   scoreCounterResult.innerHTML = score;
