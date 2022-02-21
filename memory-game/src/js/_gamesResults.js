d6624dd3df5a11e66565354a92e70ef41a94a2a1
Denis_Koval <den_hope@mail.ru>
2022-02-21 19:54:40 +0300

    feat: add number to result tabble

--- memory-game/src/js/_gamesResults.js
+++ memory-game/src/js/_gamesResults.js
@@ -29,7 +29,7 @@ const createPlayerResult = () => {
 const creareResultTabble = () => {
   let userCount = resArray.length;
   let title = document.createElement("tr");
-  title.innerHTML = `<td colspan name=tabble-title>Name</td><td colspan name=tabble-title>Score</td><td colspan name=tabble-title>Time</td><td colspan name=tabble-title>Steps</td>`;
+  title.innerHTML = `<td colspan name=tabble-title>№</td><td colspan name=tabble-title>Name</td><td colspan name=tabble-title>Score</td><td colspan name=tabble-title>Time</td><td colspan name=tabble-title>Steps</td>`;
   document.querySelector(".results-game").appendChild(title);
   for (let i = 0; i < userCount; i++) {
     let name = resArray[i].name;
@@ -37,7 +37,9 @@ const creareResultTabble = () => {
     let time = resArray[i].time;
     let steps = resArray[i].steps;
     let row = document.createElement("tr");
-    row.innerHTML = `<td colspan>${name}</td><td colspan>${score}<td colspan>${time}<td colspan>${steps}`;
+    row.innerHTML = `<td colspan>${
+      i + 1
+    }</td><td colspan>${name}</td><td colspan>${score}<td colspan>${time}<td colspan>${steps}`;
     document.querySelector(".results-game").appendChild(row);
   }
 };
