4a8c80cc5a3656ebf26d128be446bdd3fbd6bbd6
Denis_Koval <den_hope@mail.ru>
2022-03-07 16:37:22 +0300

    refactor: transfer files in repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
-const webpack = require("webpack");
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer");
-const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
-  const isProduction = options.mode === "production";
+  const isProduction = options.mode === 'production';
   const isAnalyze = env.analyze;
   const config = {
-    mode: isProduction ? "production" : "development",
-    devtool: isProduction ? "source-map" : "eval",
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'source-map' : 'eval',
     watch: !isProduction,
-    entry: ["./src/index.js", "./src/sass/style.scss"],
+    entry: ['./src/index.js', './src/sass/style.scss'],
     output: {
-      path: path.join(__dirname, "/dist"),
-      filename: "script.js",
-      assetModuleFilename: "assets/public/[hash][ext]",
+      path: path.join(__dirname, '/dist'),
+      filename: 'script.js',
+      assetModuleFilename: 'assets/public/[hash][ext]',
     },
     // optimization: {
     //   minimize: false,
     // },
     resolve: {
-      extensions: [".js", ".json", ".mjs"],
+      extensions: ['.js', '.json', '.mjs'],
     },
 
     module: {
@@ -32,43 +32,43 @@ module.exports = (env, options) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
+              presets: ['@babel/preset-env'],
             },
           },
         },
         {
           test: /\.(sa|sc|c)ss$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
         },
         {
           test: /\.(png|svg|jpe?g|gif)$/,
-          type: "asset/resource",
+          type: 'asset/resource',
         },
         {
           test: /\.html$/,
-          loader: "html-loader",
+          loader: 'html-loader',
         },
       ],
     },
     devServer: {
       open: true,
-      port: 9000,
+      port: 1000,
     },
 
     plugins: [
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
-        template: "index.html",
+        template: 'index.html',
       }),
       new MiniCssExtractPlugin({
-        filename: "style.css",
+        filename: 'style.css',
       }),
       new CopyPlugin({
         patterns: [
-          { from: "./src/assets/img", to: "./assets/img" },
-          { from: "./src/js/", to: "./assets/js" },
+          { from: './src/assets/img', to: './assets/img' },
+          { from: './src/js/', to: './assets/js' },
         ],
       }),
     ],
